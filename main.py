bf790fc4312df09141588eab47b225b54143e193
the-rigs <blainenerf@gmail.com>
2025-04-02 13:32:57 -0400

    commit

--- main.py
+++ main.py
@@ -3,6 +3,114 @@ import random
 from settings import *
 from sprites import *
 
+# class Game:
+#     def __init__(self):
+#         # initialize game window, etc
+#         pg.init()
+#         pg.mixer.init()
+#         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+#         pg.display.set_caption(TITLE)
+#         self.clock = pg.time.Clock()
+#         self.running = True
+#
+#     def new(self):
+#         # start a new game
+#         self.all_sprites = pg.sprite.Group()
+#         self.platforms = pg.sprite.Group()
+#         self.player = Player(self)
+#         self.all_sprites.add(self.player)
+#         for plat in PLATFORM_LIST:
+#             p = Platform(*plat)
+#             self.all_sprites.add(p)
+#             self.platforms.add(p)
+#         self.run()
+#
+#     def run(self):
+#         # Game Loop
+#         self.playing = True
+#         while self.playing:
+#             self.clock.tick(FPS)
+#             self.events()
+#             self.update()
+#             self.draw()
+#
+#     def update(self):
+#         # Game Loop - Update
+#         self.all_sprites.update()
+#         # check if player hits a platform - only if falling
+#         if self.player.vel.y > 0:
+#             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+#             if hits:
+#                 self.player.jumping = False
+#                 self.player.pos.y = hits[0].rect.top
+#                 self.player.vel.y = 0
+#         #if player reaches top 1/4 of screen
+#         if self.player.rect.top<=HEIGHT//4:
+#             self.player.pos.y += abs(self.player.vel.y)
+#             for plat in self.platforms:
+#                 plat.rect.y+=abs(self.player.vel.y)
+#                 if plat.rect.top >=HEIGHT:
+#                     plat.kill()
+#         #spawn new platforms average of 5
+#         while len(self.platforms)<6:
+#             width = random.randrange(50,101)
+#             x =random.randrange(0,WIDTH-width)
+#             y = random.randrange(-75, -30)
+#             p = Platform(x,y,width,20, COLOR1)
+#             self.platforms.add(p)
+#             self.all_sprites.add(p)
+#         #die
+#         if self.player.rect.top>HEIGHT:
+#             for sprite in self.all_sprites:
+#                 sprite.rect.y -= max(self.player.vel.y, 10)
+#                 # sprite.kill()
+#                 if  sprite.rect.bottom<0:
+#                     sprite.kill()
+#         #restart game
+#         if len(self.platforms) == 0:
+#             self.playing = False
+#
+#     def events(self):
+#         # Game Loop - events
+#         for event in pg.event.get():
+#             # check for closing window
+#             if event.type == pg.QUIT:
+#                 if self.playing:
+#                     self.playing = False
+#                 self.running = False
+#             if event.type == pg.KEYDOWN:
+#                 if event.key == pg.K_SPACE:
+#                     self.player.jump()
+#
+#     def draw(self):
+#         # Game Loop - draw
+#         self.screen.fill(BLACK)
+#         self.all_sprites.draw(self.screen)
+#         # *after* drawing everything, flip the display
+#         pg.display.flip()
+#
+#     def show_start_screen(self):
+#         # game splash/start screen
+#         pass
+#
+#     def show_go_screen(self):
+#         # game over/continue
+#         pass
+#
+# g = Game()
+# g.show_start_screen()
+# while g.running:
+#     g.new()
+#     g.show_go_screen()
+#
+# pg.quit()
+
+
+import pygame as pg
+import random
+from settings import *
+from sprites import *
+
 class Game:
     def __init__(self):
         # initialize game window, etc
@@ -17,12 +125,16 @@ class Game:
         # start a new game
         self.all_sprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
+        self.floors = pg.sprite.Group()
         self.player = Player(self)
         self.all_sprites.add(self.player)
         for plat in PLATFORM_LIST:
             p = Platform(*plat)
             self.all_sprites.add(p)
             self.platforms.add(p)
+        # floor = Platform(0,HEIGHT-40,WIDTH,40, RED)
+        # self.all_sprites.add(floor)
+        # self.floors.add(floor)
         self.run()
 
     def run(self):
@@ -37,38 +149,89 @@ class Game:
     def update(self):
         # Game Loop - Update
         self.all_sprites.update()
+        print("track",self.player.track)
+        print("far",self.player.far)
         # check if player hits a platform - only if falling
         if self.player.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             if hits:
-                self.player.jumping = False
                 self.player.pos.y = hits[0].rect.top
                 self.player.vel.y = 0
-        #if player reaches top 1/4 of screen
-        if self.player.rect.top<=HEIGHT//4:
-            self.player.pos.y += abs(self.player.vel.y)
+            hit_floor = pg.sprite.spritecollide(self.player,self.floors,False)
+            if hit_floor:
+                self.player.pos.y = hit_floor[0].rect.top
+                self.player.vel.y = 0
+        #if player reaches to 1/4 of screen
+        if self.player.vel.x >.05:
+            self.player.track +=1
+        if self.player.vel.x <-.05:
+
+            self.player.track -=1
+        if self.player.track > self.player.far:
+            self.player.far = self.player.track
+        if self.player.track <-50:
+            self.player.stop = True
+        if self.player.stop:
+            self.player.vel.x = 0
+        if self.player.rect.right>= 3*WIDTH/4:
+            if self.player.vel.x>.05:
+                self.player.count +=1
+
+            self.player.pos.x -= abs(self.player.vel.x)
             for plat in self.platforms:
-                plat.rect.y+=abs(self.player.vel.y)
-                if plat.rect.top >=HEIGHT:
-                    plat.kill()
+                plat.rect.x -= abs(self.player.vel.x)
+                # if self.player.count > 50:
+                #     height = random.randrange(100, 251)
+                #     x = random.randrange(WIDTH + 50, WIDTH + 150)
+                #     width = x - WIDTH
+                #     #     y = random.randrange(-75,-30)
+                #     p = Platform(x, HEIGHT - height, width, height)
+                #     self.platforms.add(p)
+                #     self.all_sprites.add(p)
+                #     self.player.count=0
+                # if plat.rect.right<=0:
+                #     height = random.randrange(100, 251)
+                #     x = random.randrange(WIDTH+50,WIDTH+150)
+                #     width = x-WIDTH
+                #     #     y = random.randrange(-75,-30)
+                #     p = Platform(x,HEIGHT-height,width,height)
+                #     self.platforms.add(p)
+                #     self.all_sprites.add(p)
+        if self.player.vel.x <-.05:
+            self.player.count -=1
+            if self.player.count <0:
+                self.player.count=0
+        if self.player.rect.left <= WIDTH/4:
+            self.player.pos.x += abs(self.player.vel.x)
+            for plat in self.platforms:
+                plat.rect.x += abs(self.player.vel.x)
+
+
+                #     plat.kill()
+
         #spawn new platforms average of 5
-        while len(self.platforms)<6:
-            width = random.randrange(50,101)
-            x =random.randrange(0,WIDTH-width)
-            y = random.randrange(-75, -30)
-            p = Platform(x,y,width,20)
-            self.platforms.add(p)
-            self.all_sprites.add(p)
+        # while len(self.platforms) < 6:
+        #     width = random.randrange(50,101)
+        #     x = random.randrange(0,WIDTH-width)
+        #     y = random.randrange(-75,-30)
+        #     p = Platform(x,y,width,20)
+        #     self.platforms.add(p)
+        #     self.all_sprites.add(p)
+
         #die
-        if self.player.rect.top>HEIGHT:
-            for sprite in self.all_sprites:
-                sprite.rect.y -= max(self.player.vel.y, 10)
-                # sprite.kill()
-                if  sprite.rect.bottom<0:
-                    sprite.kill()
-        #restart game
-        if len(self.platforms) == 0:
-            self.playing = False
+        # if self.player.rect.top> HEIGHT:
+        #     for sprite in self.all_sprites:
+        #         sprite.rect.y -= max(self.player.vel.y, 10)
+        #         if sprite.rect.bottom <0:
+        #             sprite.kill()
+        # if len(self.platforms) == 0:
+        #     self.playing = False
+
+
+
+
+
+
 
     def events(self):
         # Game Loop - events
@@ -84,7 +247,7 @@ class Game:
 
     def draw(self):
         # Game Loop - draw
-        self.screen.fill(BLACK)
+        self.screen.fill(LIGHTBLUE)
         self.all_sprites.draw(self.screen)
         # *after* drawing everything, flip the display
         pg.display.flip()
--- sprites.py
+++ sprites.py
@@ -1,97 +1,46 @@
 # Sprite classes for platform game
 import pygame as pg
 from settings import *
+import random
+vec = pg.math.Vector2
+
+import pygame as pg
+from settings import *
 vec = pg.math.Vector2
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        self.width = 0
-        self.height = 0
-        self.idle_img= []
-        self.run_img_r = []
-        self.run_img_l = []
-        self.running = False
-        self.jumping = False
-        self.scale = .8
-        self.load_images()
-        self.image = self.idle_img[0]
-        # self.image = pg.Surface((30, 40))
-        # self.image.fill(YELLOW)
+        self.image = pg.Surface((30, 40))
+        self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
-        self.last_update = 0
-        self.current_frame = 0
+        self.count = 0
+        self.track = 0
+        self.far = 0
+        self.stop = False
 
-    def animate(self):
-        now = pg.time.get_ticks()
-        if int(self.vel.x) != 0 and not self.jumping :
-            self.running = True
-        else:
-            self.running = False
-        if not self.running:
-            if now-self.last_update>125:
-                self.last_update =now
-                self.current_frame = (self.current_frame+1)%len(self.idle_img)
-                self.image = self.idle_img[self.current_frame]
-                self.rect= self.image.get_rect()
-        if self.jumping:
-            self.image = pg.image.load('idle3.png')
-            self.width = self.image.get_width()
-            self.height = self.image.get_height()
-            self.image = pg.transform.scale(self.image, (self.width * self.scale, self.height * self.scale))
-            self.rect = self.image.get_rect()
-        if self.running:
-            if now - self.last_update > 100:
-                self.last_update = now
-                self.current_frame = (self.current_frame + 1) % len(self.run_img_r)
-                if self.vel.x > 0:
-                        self.image = self.run_img_r[self.current_frame]
-                else:
-                    self.image = self.run_img_l[self.current_frame]
-                self.rect = self.image.get_rect()
-
-
-    def load_images(self):
-        for i in range(1,4):
-            filename='idle{}.png'.format(i)
-            img = pg.image.load(filename)
-            self.width = img.get_width()
-            self.height = img.get_height()
-            img =pg.transform.scale(img,(self.width*self.scale,self.height*self.scale))
-            self.idle_img.append(img)
-
-        for i in range(1,7):
-            filename='run{}.png'.format(i)
-            img = pg.image.load(filename)
-            self.width = img.get_width()
-            self.height = img.get_height()
-            img =pg.transform.scale(img,(self.width*self.scale,self.height*self.scale))
-            self.run_img_r.append(img)
-
-        for frame in self.run_img_r:
-            self.run_img_l.append(pg.transform.flip(frame,True,False))
     def jump(self):
         # jump only if standing on a platform
         self.rect.x += 1
-        hits = pg.sprite.spritecollide(self , self.game.platforms, False)
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
         self.rect.x -= 1
         if hits:
-            self.jumping = True
             self.vel.y = -20
 
     def update(self):
-        self.animate()
         self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
-            self.acc.x = -PLAYER_ACC
+            if self.stop == False:
+                self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
             self.acc.x = PLAYER_ACC
+            self.stop=False
 
         # apply friction
         self.acc.x += self.vel.x * PLAYER_FRICTION
@@ -105,12 +54,129 @@ class Player(pg.sprite.Sprite):
             self.pos.x = WIDTH
 
         self.rect.midbottom = self.pos
+# class Player(pg.sprite.Sprite):
+#     def __init__(self, game):
+#         pg.sprite.Sprite.__init__(self)
+#         self.game = game
+#         self.width = 0
+#         self.height = 0
+#         self.track = 0
+#         self.far = 0
+#         self.stop = False
+#         self.idle_img= []
+#         self.run_img_r = []
+#         self.run_img_l = []
+#         self.running = False
+#         self.jumping = False
+#         self.scale = .2
+#         self.load_images()
+#         self.image = self.idle_img[0]
+#         # self.image = pg.Surface((30, 40))
+#         # self.image.fill(YELLOW)
+#         self.rect = self.image.get_rect()
+#         self.rect.center = (0, FLOOR)
+#         self.pos = vec(0, FLOOR)
+#         self.vel = vec(0, 0)
+#         self.acc = vec(0, 0)
+#         self.last_update = 0
+#         self.current_frame = 0
+#
+#     def animate(self):
+#         now = pg.time.get_ticks()
+#         if int(self.vel.x) != 0 and not self.jumping :
+#             self.running = True
+#         else:
+#             self.running = False
+#         if not self.running:
+#             if now-self.last_update>125:
+#                 self.last_update =now
+#                 self.current_frame = (self.current_frame+1)%len(self.idle_img)
+#                 self.image = self.idle_img[self.current_frame]
+#                 self.rect= self.image.get_rect()
+#         if self.jumping:
+#             self.image = pg.image.load('idle3.png')
+#             self.width = self.image.get_width()
+#             self.height = self.image.get_height()
+#             self.image = pg.transform.scale(self.image, (self.width * self.scale, self.height * self.scale))
+#             self.rect = self.image.get_rect()
+#         if self.running:
+#             if now - self.last_update > 100:
+#                 self.last_update = now
+#                 self.current_frame = (self.current_frame + 1) % len(self.run_img_r)
+#                 if self.vel.x > 0:
+#                         self.image = self.run_img_r[self.current_frame]
+#                 else:
+#                     self.image = self.run_img_l[self.current_frame]
+#                 self.rect = self.image.get_rect()
+#
+#
+#     def load_images(self):
+#         for i in range(1,4):
+#             filename='idle{}.png'.format(i)
+#             img = pg.image.load(filename)
+#             self.width = img.get_width()
+#             self.height = img.get_height()
+#             img =pg.transform.scale(img,(self.width*self.scale,self.height*self.scale))
+#             self.idle_img.append(img)
+#
+#         for i in range(1,7):
+#             filename='run{}.png'.format(i)
+#             img = pg.image.load(filename)
+#             self.width = img.get_width()
+#             self.height = img.get_height()
+#             img =pg.transform.scale(img,(self.width*self.scale,self.height*self.scale))
+#             self.run_img_r.append(img)
+#
+#         for frame in self.run_img_r:
+#             self.run_img_l.append(pg.transform.flip(frame,True,False))
+#     def jump(self):
+#         # jump only if standing on a platform
+#         self.rect.x += 1
+#         hits = pg.sprite.spritecollide(self , self.game.platforms, False)
+#         self.rect.x -= 1
+#         if hits:
+#             self.jumping = True
+#             self.vel.y = -20
+#
+#     def update(self):
+#         self.animate()
+#         self.acc = vec(0, PLAYER_GRAV)
+#         keys = pg.key.get_pressed()
+#         if keys[pg.K_LEFT]:
+#             self.acc.x = -PLAYER_ACC
+#         if keys[pg.K_RIGHT]:
+#             self.acc.x = PLAYER_ACC
+#
+#         # apply friction
+#         self.acc.x += self.vel.x * PLAYER_FRICTION
+#         # equations of motion
+#         self.vel += self.acc
+#         self.pos += self.vel + 0.5 * self.acc
+#         # wrap around the sides of the screen
+#         if self.pos.x > WIDTH:
+#             self.pos.x = 0
+#         if self.pos.x < 0:
+#             self.pos.x = WIDTH
+#
+#         self.rect.midbottom = self.pos
 
 class Platform(pg.sprite.Sprite):
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, color):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
+        self.image.fill(color)
+        self.scale = .4
+        self.plat_img = []
+        self.load_images()
+        # self.image = random.choice(self.plat_img)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+    def load_images(self):
+        for i in range(1,5):
+            filename='platform{}.png'.format(i)
+            img = pg.image.load(filename)
+            self.width = img.get_width()
+            self.height = img.get_height()
+            img =pg.transform.scale(img,(self.width*self.scale,self.height*self.scale))
+            self.plat_img.append(img)
